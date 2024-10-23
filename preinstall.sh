01de9e1342e13dfe6d476547c93ae76e2436e2ad
YilingCAI <67104963+YilingCAI@users.noreply.github.com>
2024-10-23 14:29:16 +0200

    Update preinstall.sh
    Install eksctl

--- preinstall.sh
+++ preinstall.sh
@@ -6,3 +6,9 @@ chmod 700 get_helm.sh
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo mv /tmp/eksctl /usr/local/bin
